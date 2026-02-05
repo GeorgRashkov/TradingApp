18a9b39e8dd45b291c6e3914e41fa807d9c64364
Georg <georgrashkov79@gmail.com>
2026-02-05 17:48:34 +0200

    Creating the view of the products
    The commit adds 2 files where the first file is the view itself while the second file is the model which the view will use in order to display the products.

---
+++ TradingApp/ViewModels/Product/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TradingApp.ViewModels.Product
+{
+    public class ProductViewModel
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;        
+        public decimal Price { get; set; }
+        public string CreatorName { get; set; } = string.Empty;
+
+    }
+}
