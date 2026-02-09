a76a88fd393cf6a27d201135ce94d894a6ac42e1
Georg <georgrashkov79@gmail.com>
2026-02-09 21:29:48 +0200

    Providing the structure for the sell orders
    A new controller will empty order methods is added.  Three confirmation views have been added which are supposed to appear (currently they don't) when the user creates, cancels or buys a sell order. One sell order view model has been added which will be used by all sell orders views.

---
+++ TradingApp/Controllers/OrderController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TradingApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateSellOrder(Guid productId, int ordersCount)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateSellOrder_execute(Guid productId, int ordersCount)
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public IActionResult CancelSellOrder(Guid productId, int ordersCount)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CancelSellOrder_execute(Guid productId, int ordersCount)
+        {
+            return View();
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult BuySellOrder(Guid productId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult BuySellOrder_execute(Guid productId)
+        {
+            return View();
+        }
+    }
+}
---
+++ TradingApp/ViewModels/Order/OrderViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TradingApp.ViewModels.Order
+{
+    public class OrderViewModel
+    {
+        public string Message { get; set; } = null!;       
+        public Guid ProductId { get; set; } 
+        public int? OrdersCount { get; set; }
+    }
+}
