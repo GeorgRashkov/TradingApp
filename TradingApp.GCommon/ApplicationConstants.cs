9664f44e8243997b14ec8fb4b3155a0bd1158eaa
Georg <georgrashkov79@gmail.com>
2026-02-13 12:29:13 +0200

    Adding a class for the application constants
    The new class contains a few constants which might be used in a few layers of the app.

---
+++ TradingApp.GCommon/ApplicationConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingApp.GCommon
+{
+    public static class ApplicationConstants
+    {
+        public const int ProductsPerPage = 4;
+        public const int ProductsMaxActiveSellOrdersPerUser = 3;
+        public const int ProductMaxActiveSellOrdersPerUser = 2;
+    }
+}
