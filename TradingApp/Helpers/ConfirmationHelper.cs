687e972b8b8a97adf2316c7bea1a60d5c5b6f4a8
Georg <georgrashkov79@gmail.com>
2026-02-16 02:01:33 +0200

    Creating a confirmation partial view
    All confirmation views (`BuySellOrder`, `CancelSellOrder`, `CreateSellOrder`, `DeleteProduct`) now use a shared partial view called `_Confirmation` for displaying the information. The partial view uses a class called `ConfirmationHelper` which contains the needed data for showing the confirmation message and for executing the proper action when the user confirms. Each confirmation view makes it's own unique instantiation of `ConfirmationHelper` before passing it to the partial view.

---
+++ TradingApp/Helpers/ConfirmationHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace TradingApp.Helpers
+{
+    public class ConfirmationHelper
+    {
+        public string Message { get; set; } = null!;
+        public string Controller { get; set; } = null!;
+        public string Action { get; set; } = null!;
+        public Guid ProductId { get; set; }
+        public int? OrdersCount { get; set; }
+    }
+}
