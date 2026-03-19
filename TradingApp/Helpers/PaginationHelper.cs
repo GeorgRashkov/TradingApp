a50a6ca1b5bd5475444129eb4619b7a906b91894
Georg <georgrashkov79@gmail.com>
2026-03-19 13:42:54 +0200

    Fixing the Pagination functionality
    The pagination functionality is now shared between the Admin Area pages and the pages without area.

--- TradingApp/Helpers/PaginationHelper.cs
+++ TradingApp/Helpers/PaginationHelper.cs
@@ -4,6 +4,8 @@
     {
         public int PageIndex { get; set; }
 
+        public string? Area { get; set; }
+
         public string Controller { get; set; } = null!;
 
         public string Action { get; set; } = null!;
