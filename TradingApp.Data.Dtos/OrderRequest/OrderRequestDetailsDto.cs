0c4ad6c679714c090d8c8e52232ec80a400b71ca
Georg <georgrashkov79@gmail.com>
2026-04-01 23:52:10 +0300

    Updating the Order Request service
    The Order Request service now depends on the Order Request Repository instead of the DB Context.

---
+++ TradingApp.Data.Dtos/OrderRequest/OrderRequestDetailsDto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace TradingApp.Data.Dtos.OrderRequest
+{
+    public class OrderRequestDetailsDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public decimal MaxPrice { get; set; }
+        public string Description { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }  
+        public string CreatorUserName { get; set; } = null!;
+        public bool HasSuggestions { get; set; } 
+    }
+}
