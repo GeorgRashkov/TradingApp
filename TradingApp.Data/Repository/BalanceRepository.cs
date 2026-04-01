cbfd8e6e1e21046cce1585103f5eaa3f63c08177
Georg <georgrashkov79@gmail.com>
2026-04-02 02:02:15 +0300

    Updating the code for the balance
    The balance code is updated inside the balance repository and the balance service.

--- TradingApp.Data/Repository/BalanceRepository.cs
+++ TradingApp.Data/Repository/BalanceRepository.cs
@@ -24,24 +24,36 @@ namespace TradingApp.Data.Repository
             return balance;
         }
 
-        public async Task CreateBalanceAsync(Balance balance)
-        {
-            await _context.Balances.AddAsync(balance);
-            await _context.SaveChangesAsync();
-        }
-
+        
         public async Task<decimal> GetBalanceAmountAsync(string userId)
         {
             Balance balance = await GetBalanceAsync(userId);
             return balance.Amount;
         }
 
+        //<operation methods
+        public async Task CreateBalanceAsync(Balance balance)
+        {
+            await _context.Balances.AddAsync(balance);
+            int affectedEntities = await _context.SaveChangesAsync();
+
+            if (affectedEntities != 1)
+            {
+                throw new Exception("Failed to create Balance.");
+            }
+        }
+        
         public async Task IncreaseBalanceAsync(string userId, decimal increasement)
         {
             Balance balance = await GetBalanceAsync(userId);
 
             balance.Amount += increasement;
-            await _context.SaveChangesAsync();
+            int affectedEntities = await _context.SaveChangesAsync();
+
+            if (affectedEntities != 1)
+            {
+                throw new Exception("Failed to increase Balance.");
+            }
         }
 
         public async Task DecreaseBalanceAsync(string userId, decimal decreasement)
@@ -49,7 +61,13 @@ namespace TradingApp.Data.Repository
             Balance balance = await GetBalanceAsync(userId);
 
             balance.Amount = balance.Amount >= decreasement ? balance.Amount - decreasement : 0;
-            await _context.SaveChangesAsync();
+            int affectedEntities = await _context.SaveChangesAsync();
+
+            if (affectedEntities != 1)
+            {
+                throw new Exception("Failed to decrease Balance.");
+            }
         }
+        //operation methods>
     }
 }
