935e5e6742b9e4f483c4820cf908b8a90ec6d76b
Georg <georgrashkov79@gmail.com>
2026-04-01 16:53:53 +0300

    Fixing the balance service
    The balance service now depends on the Balance Repository and the User Manager instead of the DB Context.

--- TradingApp.Data/Repository/Interfaces/IBalanceRepository.cs
+++ TradingApp.Data/Repository/Interfaces/IBalanceRepository.cs
@@ -1,7 +1,14 @@
 ﻿
+using TradingApp.Data.Models;
+
 namespace TradingApp.Data.Repository.Interfaces
 {
     public interface IBalanceRepository
     {
+        Task<Balance> GetBalanceAsync(string userId);
+        Task CreateBalanceAsync(Balance balance);
+        Task<decimal> GetBalanceAmountAsync(string userId);
+        Task IncreaseBalanceAsync(string userId, decimal increasement);
+        Task DecreaseBalanceAsync(string userId, decimal decreasement);
     }
 }
--- TradingApp.Services.Core/BalanceService.cs
+++ TradingApp.Services.Core/BalanceService.cs
@@ -1,24 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
-using TradingApp.Data;
+﻿using Microsoft.AspNetCore.Identity;
 using TradingApp.Data.Models;
+using TradingApp.Data.Repository.Interfaces;
 using TradingApp.Services.Core.Interfaces;
 
 namespace TradingApp.Services.Core
 {
     public class BalanceService: IBalanceService
     {
-        private ApplicationDbContext _context;
-       
-        public BalanceService(ApplicationDbContext context)
-        {
-            _context = context;
+        
+        private IBalanceRepository _balanceRepository;
+        private UserManager<User> _userManager;
+
+        public BalanceService(IBalanceRepository balanceRepository, UserManager<User> userManager)
+        {           
+            _balanceRepository = balanceRepository;
+            _userManager = userManager;
         }
 
         public async Task CreateBalanceAsync(string userId)
         {
-            User? user = await _context
-                .Users
-                .FindAsync(userId);
+            User? user = await _userManager.FindByIdAsync(userId: userId);
 
             if (user == null) 
             {
@@ -31,43 +32,23 @@ namespace TradingApp.Services.Core
                 Amount = 0,
             };
 
-            await _context.Balances.AddAsync(balance);
-            await _context.SaveChangesAsync();
+            await _balanceRepository.CreateBalanceAsync(balance: balance);           
         }
 
 
         public async Task<decimal> GetUserBalanceAsync(string userId)
         {
-            Balance balance = await GetBalanceAsync(userId);
-            return balance.Amount;
+            return await _balanceRepository.GetBalanceAmountAsync(userId: userId);
         }
 
         public async Task IncreaseUserBalanceAsync(string userId, decimal increasement)
         {
-            Balance balance = await GetBalanceAsync(userId);
-
-            balance.Amount += increasement;
-             await _context.SaveChangesAsync();
+            await _balanceRepository.IncreaseBalanceAsync(userId: userId, increasement: increasement);
         }
 
         public async Task DecreaseUserBalanceAsync(string userId, decimal decreasement)
         {
-            Balance balance = await GetBalanceAsync(userId);
-
-            balance.Amount = balance.Amount >= decreasement ? balance.Amount - decreasement : 0;
-            await _context.SaveChangesAsync();
-        }
-
-        private async Task<Balance> GetBalanceAsync(string userId)
-        {
-            Balance? balance = await _context.Balances.FindAsync(userId);
-
-            if (balance == null)
-            {
-                throw new InvalidOperationException("Cannot get the balance of non existing user!");
-            }
-
-            return balance;
+            await _balanceRepository.DecreaseBalanceAsync(userId: userId, decreasement: decreasement);
         }
     }
 }
