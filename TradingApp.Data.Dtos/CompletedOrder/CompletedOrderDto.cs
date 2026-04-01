e4bf2d7e008725c97c796f9c8635e6b7282eec53
Georg <georgrashkov79@gmail.com>
2026-04-01 21:03:54 +0300

    Fixing the invoice service
    The invoice service now depends on the completed order repository instead of the DB Context.

---
+++ TradingApp.Data.Dtos/CompletedOrder/CompletedOrderDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TradingApp.Data.Dtos.CompletedOrder
+{
+    public class CompletedOrderDto
+    {
+        public Guid Id { get; set; }
+        public string BuyerId { get; set; } = null!;
+        public string TitleForBuyer { get; set; } = null!;
+        public string TitleForSeller { get; set; } = null!;
+        public DateTime CompletedAt { get; set; }
+    }
+}
